ce4187035333c126905e75f1e1f82dcc7026a843
Divyansh Seksaria <84166427+divyanshseksaria@users.noreply.github.com>
2021-05-27 10:14:40 +0530

    Create Calculate diff. between 2 numbers.c

---
+++ Calculate diff. between 2 numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter number 1: ");
+    scanf("%d", &a);
+    printf("Enter number 2: ");
+    scanf("%d", &b);
+    
+    c = a - b;
+    
+    printf("Substraction of two number = %d", c);
+    return ;
+}
